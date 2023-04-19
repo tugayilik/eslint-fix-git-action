c83b1c0d4eefce10836f02bdab6ec2c054219486
Tugay İlik <tugayilik@gmail.com>
2023-04-19 15:28:53 +0300

    Test action

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+            imports: 'always-multiline',
+            objects: 'always-multiline',
+        }],
+    },
+};
